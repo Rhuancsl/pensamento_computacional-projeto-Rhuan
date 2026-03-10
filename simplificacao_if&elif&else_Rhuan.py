fdb827933a823cdf2e8ed6dc8114944b0cf01266
Rhuancsl <rhuancs.limacontato@gmail.com>
2026-03-10 16:55:19 -0300

    update da explicacao

---
+++ simplificacao_if&elif&else_Rhuan.py
@@ -0,0 +1,14 @@
+print('qual é a sua idade?')
+
+
+idade = int(input())
+
+if idade < 18:
+    print('Você é menor de idade.')
+
+elif idade <= 65:
+    print('Você é adulto.')
+
+else:
+    print('Você é idoso de idade.')
+
